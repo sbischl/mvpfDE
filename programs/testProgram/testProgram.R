2f11c9c727cc615cc8cda6a6630a735196e20217
Simeon Bischl <s.bischl@campus.lmu.de>
2020-07-23 15:43:10 +0200

    Test Commit

--- programs/testProgram/testProgram.R
+++ programs/testProgram/testProgram.R
@@ -2,8 +2,8 @@ testProgram <- function (bootstrap_replication = 0) {
   program_name <- toString(match.call()[1])
   estimatates <- getEstimates(program_name, bootstrap_replication)
 
-  willingness_to_pay = estimatates$test_13
-  government_net_costs = estimatates$test_12
+  willingness_to_pay <- estimatates$test_12
+  government_net_costs <- estimatates$test_13
 
   return_values <- list(willingness_to_pay =  willingness_to_pay,
                         government_net_costs = government_net_costs,
