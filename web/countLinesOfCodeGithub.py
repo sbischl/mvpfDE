0985aa2cf5115a3e80873d1b1fb7c6d97fd405f7
Simeon Bischl <s.bischl@campus.lmu.de>
2020-10-07 00:32:30 +0200

    Update python script to work with any language. Apparently 100 lines of code of python are too little to show in the stats.

--- web/countLinesOfCodeGithub.py
+++ web/countLinesOfCodeGithub.py
@@ -1,5 +1,5 @@
 def countLinesOfCode():
-    user_input = askForInput()
+    user_input = askForShare()
     while True:
         try:
             code_share = float(user_input)
@@ -7,94 +7,27 @@ def countLinesOfCode():
         except ValueError:
             user_input = input("Enter a valid floating point number!")
 
+
+    user_input = askForLinesOfCode()
+    while True:
+        try:
+            lines_of_code_of_this_file = int(user_input)
+            break
+        except ValueError:
+            user_input = input("Enter a valid integer!")
+
     # Now that we have some numeric input. Lets calculate the number of lines of code:
-    lines_of_code_of_this_py_file = 100
-    lines_of_code = (lines_of_code_of_this_py_file / (code_share / 100))
+    lines_of_code = (lines_of_code_of_this_file / (code_share / 100))
     print(f'The Github repository consists of {lines_of_code:.0f} lines of code')
 
 
-def askForInput():
-    return input("What percentage of the code is in python. This is displayed on the root directory of the Github repo."
+def askForShare():
+    return input("What percentage of the code is in a programming language for which you know the number of lines of code."
+                 " This is displayed on the root directory of the Github repo."
                  " (Assuming that this file is the only file written in python)")
+
+def askForLinesOfCode():
+    return input("How many lines are written in this programming language")
 
 
 countLinesOfCode()
-
-#----------------------------------------------------------------------------------------------------------------------#
-# Whitespace below to ensure that this file has a 100 lines of code
-#----------------------------------------------------------------------------------------------------------------------#
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# This is the 100th line of code
